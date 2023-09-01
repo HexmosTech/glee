9b4ebc4841b03003c83dacd26632c6a2574c759c
Lince mathew <linz07m@gmail.com>
2023-09-01 12:07:00 +0530

    fix:install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
+
 #!/bin/bash
 
 GLEE_URL="https://github.com/HexmosTech/glee/releases/latest/download/glee.bin"
 CONFIG_URL="https://raw.githubusercontent.com/HexmosTech/glee/main/config.toml"
 
-
 DEST_DIR=""
 CONFIG_DEST_DIR="$HOME/glee"
-
 
 if [ "$(uname)" == "Darwin" ]; then
     DEST_DIR="/usr/local/bin/glee"
@@ -17,18 +16,14 @@ else
     exit 1
 fi
 
-
-
 echo "Downloading glee.bin..."
-curl -Lo glee.bin $GLEE_URL
+wget -O glee.bin $GLEE_URL
 
 # Check if the download was successful
 if [ $? -ne 0 ]; then
     echo "Failed to download glee.bin. Please check your internet connection and try again."
     exit 1
 fi
-
-
 
 echo "Moving glee.bin to $DEST_DIR..."
 sudo mv glee.bin $DEST_DIR
@@ -37,12 +32,13 @@ if [ $? -ne 0 ]; then
     echo "Failed to move glee.bin to $DEST_DIR. Please ensure you have sudo privileges."
     exit 1
 fi
+
 sudo chmod +x $DEST_DIR
 
 mkdir -p "$CONFIG_DEST_DIR"
 
 echo "Downloading config.toml..."
-curl -Lo "$CONFIG_DEST_DIR/config.toml" $CONFIG_URL
+wget -O "$CONFIG_DEST_DIR/config.toml" $CONFIG_URL
 
 echo "Installation completed successfully!"
 echo "Add the Ghost and S3 configuration in glee/config.toml file"
