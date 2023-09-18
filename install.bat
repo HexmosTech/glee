fd55d0fa79b67b4c660f0d19b381b73e3286e4ad
Lince Mathew <66767636+LinceMathew@users.noreply.github.com>
2023-09-18 20:31:00 +0530

    fix: Installation script for windows

--- install.bat
+++ install.bat
@@ -2,14 +2,24 @@
 set DOWNLOAD_URL=https://github.com/HexmosTech/glee/releases/latest/download/glee_windows.exe
 set NEW_NAME=glee.exe
 
+echo Downloading glee.exe...
 :: Download glee_windows.exe as glee.exe
 powershell -Command "(New-Object Net.WebClient).DownloadFile('%DOWNLOAD_URL%', '%NEW_NAME%')"
 
-:: Move glee.exe to C:\Windows\System32
-move glee.exe C:\Windows\System32
+:: Check if the download was successful
+if not exist "%NEW_NAME%" (
+    echo Failed to download glee.exe. Please download the executable manually from:
+    echo https://github.com/HexmosTech/glee/releases/latest
+) else (
+    echo glee.exe downloaded successfully.
+)
 
-:: Add your additional commands here
-echo glee.exe has been moved to C:\Windows\System32
 
+echo Downloading configuration file...
 
-set PATH=%PATH%;%~dp0
+:: Download .glee.toml file
+set TOML_URL=https://raw.githubusercontent.com/HexmosTech/glee/main/.glee.toml
+set TOML_DESTINATION=%USERPROFILE%\.glee.toml
+powershell -Command "(New-Object Net.WebClient).DownloadFile('%TOML_URL%', '%TOML_DESTINATION%')"
+echo .glee.toml file downloaded successfully.
+echo Add the Ghost Configuration in %USERPROFILE%\.glee.toml file
