a10cdb7c7684742655c0aed0c3218957ff3c970a
Lince mathew <linz07m@gmail.com>
2023-09-22 21:27:51 +0530

    fix: fix for s3 urls downloading image instead of opening

--- s3.py
+++ s3.py
@@ -18,8 +18,13 @@ def upload_to_s3(local_filepath, s3_filepath, logging):
             aws_access_key_id=ACCESS_KEY_ID, aws_secret_access_key=SECRET_ACCESS_KEY
         )
         s3 = session.resource("s3")
-        result = s3.Bucket(BUCKET_NAME).upload_file(local_filepath, s3_filepath)
+        bucket = s3.Bucket(BUCKET_NAME)
 
+        result = bucket.upload_file(local_filepath, s3_filepath)
+
+        bucket.Object(s3_filepath).put(ContentType="image/jpeg")
+
+        image_link = f"{S3_BASE_URL}{s3_filepath}"
         return image_link
     except Exception as e:
         sys.exit(f"Error: {e}")
